dffdd06ca82437a0b44af21b09c37173115c910a
RidaFatima03 <ridaafatima03@gmail.com>
2023-12-15 11:03:25 +0300

    connected firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,6 +38,7 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("com.google.ar.sceneform:filament-android:1.17.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
